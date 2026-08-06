1492b1ac3dbb044fe1766bb61fd3ccc8c73eb722
SAIRAM CHARI <sairamchari@iitb.ac.in>
2026-08-06 21:38:52 +0530

    Add: k-closest-points-to-origin - submission-3

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-3.cpp
@@ -0,0 +1,25 @@
+#include <queue>
+using namespace std;
+class Solution {
+public:
+    struct Comp {
+        bool operator() (vector<int>& a ,vector<int>& b){
+            return a[0]*a[0] + a[1]*a[1] < b[0]*b[0] + b[1] * b[1] ;
+        }
+    };
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<vector<int>, vector<vector<int>>, Comp> heap;
+        for (auto p : points){
+            heap.push(p);
+            if (heap.size()> k){
+                heap.pop();
+            }
+        }
+        vector<vector<int>> result;
+        while(!heap.empty()){
+            result.push_back(heap.top());
+            heap.pop();
+        }
+        return result;
+    }
+};
